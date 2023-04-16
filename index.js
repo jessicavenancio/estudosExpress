858ccee1d7497abcb5bf5c581cb9e0d3937116df
jessicavenancio <jessicavenancioti@gmail.com>
2023-04-16 15:05:45 -0300

    Atividades finalizadas

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const students = require("./students");
 
-
 //Config do app
 const app = express();
+/*Desafio 4: Pesquise e aplique o logger morgan na aplicação;*/
+const morgan = require('morgan');
 app.use(express.json());
 
 //Rotas
-
-/* Crie uma rota GET para “/alunos” que lista todos os alunos. 
+/*1- Crie uma rota GET para “/alunos” que lista todos os alunos. 
 Deve conter query opcional para filtrar por nome e por média.
 Ou seja, a rota pode ter este formato: “/alunos?nome=pedro”, “/alunos?media=7.5” ou esse “/alunos”.
 Esta rota deve utilizar as funções exportadas pelo módulo alunos.js;*/
@@ -18,9 +18,9 @@ app.get("/students", (req, res) => {
     res.json(list);
 });
 
-
-/*Crie uma rota POST para “/alunos/novo” e o corpo da requisição deve conter (nome, matrícula e média). 
+/*2- Crie uma rota POST para “/alunos/novo” e o corpo da requisição deve conter (nome, matrícula e média). 
 Valide os campos passados e caso contrário indique um erro (400);*/
+/*Desafio 1: Refatore a aplicação e mova para alunos.js, os métodos de deletar, adicionar e atualizar um aluno;*/
 app.post("/students/new", (req, res) => {
     const { name, register, average } = req.body;
     if (!name || !register || !average) {
@@ -32,7 +32,7 @@ app.post("/students/new", (req, res) => {
     }
 });
 
-/*Crie uma rota POST para “/alunos/deletar/:index” que indica qual aluno remover do array de dados (index).
+/*3- Crie uma rota POST para “/alunos/deletar/:index” que indica qual aluno remover do array de dados (index).
 Trate a chamada se o aluno não existir (404);*/
 app.post("/students/delete/:index", (req, res) => {
     const index = req.params.index;
@@ -45,7 +45,50 @@ app.post("/students/delete/:index", (req, res) => {
     }
 });
 
+/*Crie uma rota POST para /alunos/atualizar/:index, que no corpo da requisição recebe um objeto (nome, média)
+e atualiza os dados do aluno naquela posição. 
+Trate a chamada se o aluno não existir (404);*/
+/* Desafio 0: Escreva um arquivo JSON chamado db.json toda vez que ocorrer uma alteração nos dados do array de alunos;*/
+app.post("/students/update/:index", (req, res) => {
+    const index = parseInt(req.params.index);
+    const { name, average } = req.body;
+    const updateFile = students.updateFile(index, name, average);
 
+    if (updateFile) {
+        //res.status(200).send('Atualização realizada!');
+        res.json(updateFile);
+    } else {
+        res.status(404).send('Estudante não encontrado!');
+    }
+});
+
+/*Desafio 2: Substituir as rotas POST de atualizar e deletar com os métodos PUT e DELETE respectivamente,
+reformulando as URLs para todas utilizarem o mesmo caminho /alunos, mudando apenas o método utilizado;*/
+//atualizar
+app.put("/students/:index", morgan('tiny'), (req, res) => {
+    const index = Number(req.params.index);
+    const { name, average } = req.body;
+    const updateFile = students.updateFile(index, name, average);
+
+    if(updateFile) {
+        res.json(updateFile);
+    } else {
+        res.status(404).json({mensagem: "Aluno não encontrado"})
+    }
+});
+//deletar
+app.delete("/students/:index", morgan('tiny'), (req, res) => {
+    const index = Number(req.params.index);
+    const deleteStudents = students.deleteStudents(index);
+
+    if(deleteStudents) {
+        res.json(deleteStudents);
+    } else {
+        res.status(404).json({mensagem: "Aluno não encontrado"})
+    }
+});
+
+/*Desafio 3: Entregue a documentação desta API usando os recursos do Postman; ok*/
 
 //Escuta das requisições
 app.listen(3000, () => {
--- students.js
+++ students.js
@@ -6,8 +6,7 @@ const students = [
 ];
 
 
-function listStudents( name, average )
-{
+function listStudents(name, average) {
     let listFilter = students;
     if (name) {
         listFilter = listFilter.filter(s => s.name.toLowerCase().includes(name.toLowerCase()));
@@ -18,10 +17,41 @@ function listStudents( name, average )
     return listFilter;
 }
 
-function deleteStudents(index)
-{
+function deleteStudents(index) {
     const studentsRemoved = students.splice(index, 1);
     return studentsRemoved.length > 0 ? studentsRemoved[0] : null;
 }
 
-module.exports = {students, listStudents, deleteStudents};
+const fs = require('fs');
+const path = require('path');
+
+function updateFile(index, name, average) {
+    if (index >= 0 && index < students.length) {
+        const updateFile = students[index];
+        updateFile.name = name || updateFile.name;
+        updateFile.average = average || updateFile.average;
+
+        //Desafio 0:
+        fs.writeFile(
+            path.join(__dirname, 'db.json'),
+            JSON.stringify(students),
+            err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Dados atualizados!');
+                }
+            }
+        );
+
+        return updateFile;
+    } else {
+        return null
+    }
+};
+
+
+
+
+
+module.exports = { students, listStudents, deleteStudents, updateFile};
